252cdafef9b320a76da72e5d7020d8d908e39669
Oliver Coimbra <olivercoimbra@gmail.com>
2021-11-17 14:17:25 -0400

    Fix errors linter

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const menu = document.querySelector('.menu');
 const btn = document.querySelector('.menu-btn');
 const btnClose = document.querySelector('.menu-close');
-const selectMenu = document.querySelector('.menu-i');
 
 btn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    btnClose.classList.add('close-act');
-    btn.classList.add('menu-btn-close');
-})
+  menu.classList.toggle('active');
+  btnClose.classList.add('close-act');
+  btn.classList.add('menu-btn-close');
+});
 
 btnClose.addEventListener('click', () => {
-    menu.classList.remove('active');
-    btnClose.classList.remove('close-act');
-    btn.classList.remove('menu-btn-close');
-})
+  menu.classList.remove('active');
+  btnClose.classList.remove('close-act');
+  btn.classList.remove('menu-btn-close');
+});
 
 menu.addEventListener('click', () => {
-    console.log('dieron clic');
-    menu.classList.remove('active');
-    btnClose.classList.remove('close-act');
-    btn.classList.remove('menu-btn-close');
-}) 
+  menu.classList.remove('active');
+  btnClose.classList.remove('close-act');
+  btn.classList.remove('menu-btn-close');
+});
