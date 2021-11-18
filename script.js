4780099fd3d44bec102fee3e719f01caae8af954
Oliver Coimbra <olivercoimbra@gmail.com>
2021-11-18 14:10:18 -0400

    Fix linter errors

--- script.js
+++ script.js
@@ -23,49 +23,48 @@ menu.addEventListener('click', () => {
 document.getElementById('project-1').addEventListener('click', () => {
   document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
 document.getElementById('project-2').addEventListener('click', () => {
-  console.log('hizo click');
   document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
 document.getElementById('project-3').addEventListener('click', () => {
   document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
 document.getElementById('project-4').addEventListener('click', () => {
   document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
 document.getElementById('project-5').addEventListener('click', () => {
   document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
 document.getElementById('project-6').addEventListener('click', () => {
   document.getElementById('modal-popup').classList.add('popup-visible');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.add('popup-transparency');
+    document.getElementById('modal-popup').classList.add('popup-transparency');
   }, 300);
 });
 
 document.getElementById('close-popup').addEventListener('click', () => {
   document.getElementById('modal-popup').classList.remove('popup-transparency');
   setTimeout(() => {
-  document.getElementById('modal-popup').classList.remove('popup-visible');
+    document.getElementById('modal-popup').classList.remove('popup-visible');
   }, 300);
 });
